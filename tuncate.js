cf26824b8e59ce235de3656b116ea4d7c1261fe7
Mithun Kumar S R <mithunkumar.sr@uber.com>
2019-12-14 21:23:17 +0530

    Create tuncate.js

---
+++ tuncate.js
@@ -0,0 +1,31 @@
+(function(exports) {
+
+  'use strict';
+
+  function truco(string, limit, options) {
+    limit = limit || string.length;
+    options = options || {};
+
+    var ellipsis = options.ellipsis || '…';
+    var moonwalk = options.moonwalk || false;
+
+    if(string.length <= limit) {
+      return string;
+    }
+
+    if(moonwalk) {
+      return ellipsis + string.substring(string.length - limit);
+    } else {
+      return string.substring(0, limit) + ellipsis;
+    }
+  }
+
+  if(typeof define === 'function' && define.amd) {
+    define(function() { return truco; });
+  } else if(typeof module !== 'undefined' && module.exports) {
+    module.exports = truco;
+  } else {
+    exports.truco = truco;
+  }
+
+})(this);
